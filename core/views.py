1ad4c0459b35cf6c0f0078fc60df3e1ec987f20e
zevitagem <jose.vsilva@mercadolivre.com>
2022-12-14 16:21:11 -0300

    Added static example

--- core/views.py
+++ core/views.py
@@ -6,7 +6,7 @@ from django.views import generic
 
 
 class IndexView(generic.ListView):
-    template_name = 'polls/index.html'
+    template_name = 'core/index.html'
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
